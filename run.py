89dbb00cee3bc89bfb1e7f5c876990e5609955e1
Ian Buttimer <ibuttimer@yahoo.com>
2022-07-08 12:57:54 +0100

    maint: Heroku deployment

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+loop = True
+
+print('Hello, world!')
+
+while loop:
+    data = input("Enter something: ")
+    loop = data.lower() != 'quit'
+    if loop:
+        print("Entered: " + data)
